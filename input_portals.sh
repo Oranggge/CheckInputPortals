9a8b19b8a62c9e9c799a6ffe474d88f663cb6b3e
theklim <github@theklim.com>
2023-01-29 21:55:39 +0100

    list the latest modified file in folder

--- input_portals.sh
+++ input_portals.sh
@@ -7,9 +7,12 @@ input="/home/mlappi/job/bash_scripts/path.txt"
 while read -r line
 do
   echo "$line"
-  for file in "$line"/*
-  do 
-    echo -n "$file"
-    date -r "$file"
-  done
+#  for file in "$line"/*
+#  do 
+#    echo -n "$file"
+#    date -r "$file"
+#    ls -tl "$file"| sed -n '2{p;}'
+#   list all files in reverse time order| exclude all folders | list the first file (first line is ignored)
+    ls -tl "$line"| grep -v ^d| sed -n '2{p;q}'
+#  done
 done < "$input"
